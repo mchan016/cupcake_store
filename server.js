dade6a44a960a0310077a2c50ad5e8a695ffa172
Michael Chan <mchan016@gmail.com>
2024-02-04 13:33:20 -0800

    cupcake v1.0 - Add connection to atlas database

--- server.js
+++ server.js
@@ -1,6 +1,15 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+dotenv.config({path: './config.env'});
 const app = require('./app');
 
-const port = 8000;
+// TODO: Refactor to connect locally once api server functionality works
+// Connect to database
+const DB = process.env.ATLAS.replace('<PASSWORD>', process.env.ATLAS_PASSWORD);
+mongoose.connect(DB)
+    .then(() => console.log('DB Connection successful!'));
+
+const port = process.env.PORT || 8000;
 app.listen(port, () => {
     console.log(`App running on port ${port}`);
 });
