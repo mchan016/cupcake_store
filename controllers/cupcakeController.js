8af6e03cb15d5a04d583ff28949ac57a639b1a1f
Michael Chan <mchan016@gmail.com>
2024-02-04 14:26:49 -0800

    cupcake v1.0 - Implement functionality for controllers

--- controllers/cupcakeController.js
+++ controllers/cupcakeController.js
@@ -1,21 +1,94 @@
+const Cupcake = require('./../models/cupcakeModel');
 
+exports.listCupcakes = async (req, res) => {
+    try {
+        const cupcakes = await Cupcake.find();
 
-exports.listCupcakes = (req, res) => {
-    res.send('Getting all cupcakes...');
+        res.status(200).json({
+            status: 'success',
+            count: cupcakes.length,
+            data: {
+                cupcakes
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 };
 
-exports.addCupcake = (req, res) => {
-    res.send('Adding a new cupcake...');
+exports.addCupcake = async (req, res) => {
+    try {
+        const cupcake = await Cupcake.create(req.body);
+
+        res.status(201).json({
+            status: 'success',
+            data: {
+                cupcake
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 };
 
-exports.getCupcakeById = (req, res) => {
-    res.send('Getting your designated cupcake...');
+exports.getCupcakeById = async (req, res) => {
+    try {
+        const cupcake = await Cupcake.findById(req.params.id);
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                cupcake
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 };
 
-exports.updateCupcake = (req, res) => {
-    res.send('Updating your cupcake');
+exports.updateCupcake = async (req, res) => {
+    try {
+        // Get the updated cupcake record
+        const cupcake = await Cupcake.findByIdAndUpdate(req.params.id, req.body, {
+            new: true,
+            runValidators: true
+        });
+
+        res.status(200).json({
+            status: 'success',
+            data: {
+                cupcake
+            }
+        });
+    } catch (err) {
+        res.status(404).json({
+            status: 'fail',
+            message: err
+        });
+    }
 };
 
-exports.deleteCupcake = (req, res) => {
-    res.send('Delete a cupcake');
+exports.deleteCupcake = async (req, res) => {
+    try {
+        await Cupcake.findByIdAndDelete(req.params.id);
+
+        res.status(204).json({
+            status: 'success',
+            data: null
+        });
+    } catch (err) {
+        res.status(400).json({
+            status: 'fail',
+            message: err
+        });
+    }
 };
